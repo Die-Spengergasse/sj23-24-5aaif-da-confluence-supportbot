a86f2342edfd006709a3d89d73e31c5e5ad59fda
Can Aksoy <aks210921@spengergasse.at>
2024-06-10 15:35:39 +0200

    AddedFrontendJulian

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,148 @@
 <script setup>
-import { useUserStore } from '@/stores/user'
-import { RouterLink, RouterView } from 'vue-router'
+import { useUserStore } from "@/stores/user";
+import { RouterLink, RouterView } from "vue-router";
 
 const user = useUserStore();
 </script>
 
 <template>
-  <header>
-    <nav>
-      <h5 class="navbarHeader">Angemeldet als {{ user.userdata.username }}
-            <small>({{ user.userdata.guid }})</small>
-          </h5>
-    </nav>
-  </header>
+  <div class="patter">
+    <header>
+      <div class="flex-container">
+        <!-- <div class="item1">
+          <nav class="leftside">
+            <a>Terms of Service</a>
+            <a>Help</a>
+          </nav>
+        </div> -->
+
+        <div class="item2">
+          <a>Confluence SupportBot</a>
+          <nav class="Profil">
+            <img src="@/assets/profil_icon.png" />
+            <h5 class="navbarHeader">
+              Angemeldet als {{ user.userdata.username }}
+              <!-- <small>({{ user.userdata.guid }})</small> -->
+            </h5>
+          </nav>
+        </div>
+
+        <!-- <div class="item3">
+          <nav class="rightside">
+            <a>Team</a>
+            <a>Impressum</a>
+          </nav>
+        </div> -->
+      </div>
+    </header>
+
+    <div class="einfuehrung">
+      <p>
+        Nutze den SupportBot, um Dokumentationen schneller und effizienter zu
+        finden! <br />Er erleichtert die Suche erheblich und spart dir wertvolle
+        Zeit.
+      </p>
+    </div>
+
+    <div class="bot_icon">
+      <img src="@/assets/bot_icon.png" />
+    </div>
+  </div>
   <main>
     <RouterView />
   </main>
 </template>
 
 <style scoped>
+header {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: center;
+}
 
+nav {
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+}
+
+nav a {
+  color: #fff;
+  text-decoration: none;
+  text-align: center;
+  align-items: center;
+  padding-right: 40px;
+}
+
+.flex-container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.flex-container > div {
+  border-radius: 5px;
+  padding: 8px;
+  padding-right: 40px;
+}
+
+.item1 {
+  flex-grow: 2;
+  align-items: center;
+  text-align: center;
+  font-family: "Trebuchet MS";
+}
+
+.item2 {
+  flex-grow: 1;
+  font-size: 30px;
+  font-family: "Trebuchet MS";
+  font-weight: 90000;
+}
+
+.item3 {
+  flex-grow: 2;
+  align-self: auto;
+  font-family: "Trebuchet MS";
+}
+
+.Profil {
+  block-size: 30px;
+  justify-items: flex-end;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-size: 20px;
+}
+
+.Profil img {
+  block-size: 30px;
+}
+
+.einfuehrung {
+  font-family: "Trebuchet MS";
+  font-weight: 800;
+  text-align: center;
+  padding-block: 40px;
+}
+
+.bot_icon {
+  block-size: 150px;
+  background-color: #007bff;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+
+  border-top-left-radius: 80px;
+  border-top-right-radius: 80px;
+}
+
+.bot_icon img {
+  height: 80%;
+  border: 5px solid #000;
+  background-color: #fff;
+  border-radius: 50%;
+}
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,24 @@
 <script setup>
-import axios from 'axios';
+import axios from "axios";
 </script>
 
 <template>
   <div class="home">
-    <h1>HomeView</h1>
-    <textarea v-model="query" placeholder="Suchanfrage"></textarea>
-    <button v-on:click="sendQuery()">Senden</button>
+    <textarea
+      class="texteingabe"
+      v-model="query"
+      placeholder="Netzwerkproblem..."
+    ></textarea>
+    <button class="senden_button" v-on:click="sendQuery()">Suchen</button>
+  </div>
+
+  <div class="ausgabe">
     <template v-if="results.length">
       <h2>Ergebnisse</h2>
       <ul>
-        <li v-for="result in results" v-bind:key="result.title">
-          {{ result }}
+        <li v-for="result in results" :key="result.title">
+          <h3>{{ result.title }}</h3>
+          <h6>{{ result.content }}</h6>
         </li>
       </ul>
     </template>
@@ -19,26 +26,92 @@ import axios from 'axios';
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       query: "",
-      results: []
+      results: [],
     };
-  },
-  async mounted() {
-
   },
   methods: {
     async sendQuery() {
-      const response = await axios.get("search", { params: { query: this.query } });
+      const response = await axios.get("search", {
+        params: { query: this.query },
+      });
       this.results = response.data;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
+.ausgabe h2 {
+  padding-left: 47px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  padding-left: 35px;
+  padding-right: 35px;
+}
+
+li {
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 20px; /* Abgerundete Ecken für die Listenelemente */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Leichter Schatten für jedes Element */
+  border: 5px solid gray;
+}
+
+li h3 {
+  color: #007bff; /* Blaue Schrift für Titel */
+  font-weight: bold;
+}
+
+.home {
+  block-size: 85px;
+  background-color: #007bff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+}
+.ausgabe {
+  border-bottom-left-radius: 80px;
+  border-bottom-right-radius: 80px;
+  border: 30px solid #007bff;
+  box-sizing: border-box;
+}
+.texteingabe {
+  border-bottom-left-radius: 80px;
+  border-top-left-radius: 80px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  outline: none;
+  width: 350px;
+  box-sizing: border-box;
+  resize: none;
+  height: 40px;
+}
+
+.senden_button {
+  border-top-right-radius: 80px;
+  border-bottom-right-radius: 80px;
+  padding: 0;
+  height: 40px;
+  border: none;
+  background-color: #ffffff;
+  color: #000;
+  cursor: pointer;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+}
+
 .error {
   color: red;
   font-size: 80%;
